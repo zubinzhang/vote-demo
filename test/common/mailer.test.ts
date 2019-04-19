563c0cc17b8d438673e0c15deb2527303b98803a
Zubin <zubincheung@outlook.com>
2019-04-19 21:38:05 +0800

    update:mailer test

--- test/common/mailer.test.ts
+++ test/common/mailer.test.ts
@@ -5,7 +5,7 @@ import Mailer from '../../src/common/mailer';
 describe('/test/common/mailer.test.ts', () => {
   it('#sendMail', async () => {
     const mailer = await app.applicationContext.getAsync<Mailer>('mailer');
-    const data = await mailer.sendMail({ userId: 3, email: 'zubincheung@163.com' });
+    const data = await mailer.sendMail({ userId: 3, email: 'test@163.com' });
     assert(!!data);
   });
 });
